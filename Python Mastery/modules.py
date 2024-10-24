9320dfcb27820ec8546a259ec3a4afcd7023ecab
Moeen Ahmad <131704973+itsmoeenahmad@users.noreply.github.com>
2024-10-24 21:26:43 +0500

    Completed

---
+++ Python Mastery/modules.py
@@ -0,0 +1,15 @@
+def hello():
+    print('hello')
+
+def hy():
+    print('hyy')
+
+name = 'ali'
+
+message = 'This is message.....'
+
+def simple():
+    print('Simple')
+
+def demo():
+    print('Demo')
